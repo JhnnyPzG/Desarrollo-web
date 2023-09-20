cdde181fd5406bc2b2a7a34264a051cfa66b58c3
JhnnyPzG <jpaz152@soyudemedellin.edu.co>
2023-09-19 19:32:47 -0500

    Resubir

---
+++ Barra-Boostrap/index.js
@@ -0,0 +1,15 @@
+console.log("Borojo")
+
+const edad = 55;
+let nombre = "Carmelo";
+const estudiante = false;
+const altura = 1.90;
+const semestres = [];
+const materiaP = {nombre: "Calculo", id: 0};
+const universidad = null; 
+const carrera = undefined;
+
+console.log(typeof 5);
+const precioMatricula = "50000000.5";
+console.log(parseInt(precioMatricula))
+console.log(parseFloat(precioMatricula))
